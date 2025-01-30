ac764cad63524dff6991c116c8b03d4330aacd5b
Morozov Igor <morozovigorq@gmail.com>
2025-01-30 21:49:15 +0300

    feat: meked swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  const config = new DocumentBuilder().setTitle('Telegram Phone Image Store').setVersion('0.1').build()
+  const documentFactory = () => SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('docs', app, documentFactory)
   await app.listen(3000)
 }
 bootstrap()
